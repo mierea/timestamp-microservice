0ecbd14954bb280b88139c3e99070f2ed6e3acb1
mhm <mierea.ciprian@gmail.com>
2016-03-05 22:58:24 +0000

    Added index.js, package.json and public/index.html

---
+++ index.js
@@ -0,0 +1,26 @@
+ var express = require("express");
+ var app = express();
+
+ var moment = require("moment");
+ /* serves main page */
+ app.get("/", function(req, res) {
+    res.sendFile('public/index.html')
+ });
+
+
+app.get('/:query', function(req, res) {
+        var date = req.params.query;
+		res.setHeader('Content-Type', 'application/json');
+		if(!isNaN(parseInt(date))){
+			 res.send({"unix": parseInt(date), "natural": moment.unix(date).format("MMMM D, YYYY")});
+		} else if (moment(date, "MMMM D, YYYY").isValid()){
+			 res.send({"unix":parseInt(moment.unix(moment(date, "MMMM D, YYYY").unix()).format("X")), "natural": date});
+		}
+		else {
+			res.send({"unix":null,"natural":null});
+		}
+		});
+ var port = process.env.PORT || 8080;
+ app.listen(port, function() {
+   console.log("Listening on " + port);
+ });
